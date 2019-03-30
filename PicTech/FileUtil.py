3f33881e6c862ca846c2f3f8d4da03a341be4a6f
Enmin zhou <enmin.zhou@deepinv.com>
2019-03-30 12:34:16 -0700

    add key points graph function

--- PicTech/FileUtil.py
+++ PicTech/FileUtil.py
@@ -1,5 +1,5 @@
 import configparser
-
+from PIL import Image
 
 def readConfigSections(filename):
 	cf = configparser.ConfigParser()
@@ -20,3 +20,23 @@ def readPicture(filename):
 	with open(filename, 'rb') as f:
 		image = f.read()
 	return image
+
+
+def getKeyPointsGraph(filename, bodyAnalysisData):
+	img = Image.open(filename)
+	data = bodyAnalysisData['person_info'][0]['body_parts']
+	plist = []
+	Green = (220, 20, 60)
+	Blue = (0, 0, 255)
+	for points in data:
+		pixel = (int(data[points]['x']), int(data[points]['y']))
+		for i in range(0, 10):
+			for j in range(0, 10):
+				element = (pixel[0] + i, pixel[1] + j)
+				if points == 'right_hip' or points == 'left_hip' or points == 'right_shoulder' or points == 'left_shoulder':
+					plist.append((element, Blue))
+				else:
+					plist.append((element, Green))
+	for p in plist:
+		img.putpixel(p[0], p[1])
+	return img
